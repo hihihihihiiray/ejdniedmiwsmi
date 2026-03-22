98d72f468fbb594818f18d6877150b547be6bb5c
hihihihihiiray <raymondvu879@gmail.com>
2026-03-21 21:31:46 -0500

    Update videasy.js

--- providers/videasy.js
+++ providers/videasy.js
@@ -9,7 +9,7 @@ const HEADERS = {
 
 // VideoEasy API configuration
 const API = 'https://enc-dec.app/api';
-const TMDB_API_KEY = 'd131017ccc6e5462a81c9304d21476de';
+const TMDB_API_KEY = '1c29a5198ee1854bd5eb45dbe8d17d92'
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 
 // VideoEasy server configurations
